5128ccdc85ae76a9273551f9d01e991328d2eb30
Rene Valladares <reneakuma@hotmail.com>
2024-05-07 21:18:55 -0600

    Adjusting some sintaxis

--- AnalisisExploratorio.R
+++ AnalisisExploratorio.R
@@ -1,22 +1,22 @@
-#Carga del archivo separa por comas en la variable melateData
-melateData<-read.csv("ResultadosMelate.csv")
-#Muestra el nombre de las columnas
-names(melateData)
-#Muestra contenido de las observaciones
-melateData
-#Muestra el resumen de los datos
-summary(melateData)
-#Muestra las dimensiones de los datos
-dim(melateData)
-#Muestra el histograma de la variable N1
-hist(melateData$N1)
-#Muestra el histograma de la variable N2
-hist(melateData$N2)
-#Muestra el histograma de la variable N3
-hist(melateData$N3)
-#Muestra el histograma de la variable N4
-hist(melateData$N4)
-#Muestra el histograma de la variable N5
-hist(melateData$N5)
-#Muestra el histograma de la variable N6
-hist(melateData$N6)
+# Carga del archivo separa por comas en la variable melateData
+melate_data <- read.csv("ResultadosMelate.csv")
+# Muestra el nombre de las columnas
+names(melate_data)
+# Muestra contenido de las observaciones
+melate_data
+# Muestra el resumen de los datos
+summary(melate_data)
+# Muestra las dimensiones de los datos
+dim(melate_data)
+# Muestra el histograma de la variable N1
+hist(melate_data$N1)
+# Muestra el histograma de la variable N2
+hist(melate_data$N2)
+# Muestra el histograma de la variable N3
+hist(melate_data$N3)
+# Muestra el histograma de la variable N4
+hist(melate_data$N4)
+# Muestra el histograma de la variable N5
+hist(melate_data$N5)
+# Muestra el histograma de la variable N6
+hist(melate_data$N6)
